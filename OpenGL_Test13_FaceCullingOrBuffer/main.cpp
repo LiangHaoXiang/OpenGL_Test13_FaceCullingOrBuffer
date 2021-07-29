f06c5f7c50af64a9cb07d4d6b0f9b078e6143a79
LiangHaoXiang <1013568397@qq.com>
2021-07-29 09:38:20 +0800

    Initial Commit

---
+++ OpenGL_Test13_FaceCullingOrBuffer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL_Test13_FaceCullingOrBuffer
+//
+//  Created by Haoxiang Liang on 2021/7/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
